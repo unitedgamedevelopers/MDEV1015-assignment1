fc417c1d9fabaf79f638795db5f094913a01060b
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2023-02-24 23:32:56 -0500

    refactor: refactor User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,14 @@
 import FirebaseService from '../services/FirebaseService';
 
-interface User {
-  uid: string;
+interface IUser {
   email: string;
   password: string;
 }
 
-class User {
+class User implements IUser {
+  email: string;
+  password: string;
+
   constructor(email: string, password: string) {
     this.email = email;
     this.password = password;
