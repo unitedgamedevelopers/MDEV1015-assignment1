235b4205c3c722bc4f6c20e0f24238b9b5779231
mahfuz-aftab <mahfuz.cracker@gmail.com>
2023-04-19 18:44:29 -0400

    feat: add auth provider to app

--- App.tsx
+++ App.tsx
@@ -4,21 +4,24 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import LoginScreen from './src/views/LoginScreen';
 import HomeScreen from './src/views/HomeScreen';
+import AuthProvider from './src/providers/AuthProvider';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Login"
-          component={LoginScreen}
-        />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Login"
+            component={LoginScreen}
+          />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
 
